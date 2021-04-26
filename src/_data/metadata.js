a15e63d3ea6b87043127664f2172cfbc0c4bc6a3
Jason Rose <jasoncrose+ubuntu2004@gmail.com>
2021-04-25 17:07:23 -0700

    11ty setup

---
+++ src/_data/metadata.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: "Title of the website. Update me.",
+  url: "https://update-me.com",
+  domain: "update-me.com",
+  description: "Description of the website. Update me.",
+  genre: "Insert a schema.org genre",
+  googleAnalyticsId: "GA ID. Update me. Note, that this is not compatible with the not-yet-commonly used version 4 of Google Analytics",
+  sendWebVitals: false,
+  feed: {
+    subtitle: "Subtitle. Update me.",
+    filename: "feed.xml",
+    path: "/feed/feed.xml",
+    id: "https://update-me.com"
+  },
+  jsonfeed: {
+    path: "/feed/feed.json",
+    url: "https://update-me.com/feed/feed.json"
+  },
+  author: {
+    name: "Author Name. Update me.",
+    twitterHandle: "update_me"
+  },
+  publisher: {
+    name: "Publisher name. Update me."
+  }
+}
